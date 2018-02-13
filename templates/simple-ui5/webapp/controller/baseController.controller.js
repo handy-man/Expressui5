692f668379e22d222bf6bd4a0b099df3bb01b5d5
Nathan <nathan454@hotmail.co.uk>
2018-02-13 11:20:28 +0000

    add base controller, rename vie1 to mainView

--- templates/simple-ui5/webapp/controller/baseController.controller.js
+++ templates/simple-ui5/webapp/controller/baseController.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("ui5expresstemplate.controller.View1", {
+	return Controller.extend("ui5expresstemplate.controller.baseController", {
 
 	});
 });
---
+++ templates/simple-ui5/webapp/controller/mainView.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"templatepath/controller/BaseController"
+], function(baseController) {
+	"use strict";
+
+	return baseController.extend("ui5expresstemplate.controller.mainView", {
+
+	});
+});
