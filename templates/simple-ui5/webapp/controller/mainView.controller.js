239e47e455fadc90750330c316f8bf164de0e7de
Nathan <nathan454@hotmail.co.uk>
2018-02-14 14:21:58 +0000

    add in the basic functions to the simple app template, not required but useful to new users.

--- templates/simple-ui5/webapp/controller/mainView.controller.js
+++ templates/simple-ui5/webapp/controller/mainView.controller.js
@@ -5,10 +5,35 @@ sap.ui.define([
 
 	return baseController.extend("ui5expresstemplate.controller.mainView", {
 
-		/*
-		* The onInit function is called when this controller is called, mostly used to define methods to call on route matched.
-		*/
-        onInit: function () {
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 */
+		onInit: function() {
+
+		},
+
+		/**
+		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+		 * (NOT before the first rendering! onInit() is used for that one!).
+		 */
+		onBeforeRendering: function() {
+
+		},
+
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+		 * This hook is the same one that SAPUI5 controls get after being rendered.
+		 */
+		onAfterRendering: function() {
+
+		},
+
+		/**
+		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+		 */
+		onExit: function() {
+
 		}
 	});
 });
